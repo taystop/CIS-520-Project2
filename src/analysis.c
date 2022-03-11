9904eac78ea63da7c9bea2780620d28afca84d2d
Tayler Stoppel <stoppel@eos.beocat.ksu.edu>
2022-03-11 14:40:02 -0600

    Basic functionality of Analysis.c

--- src/analysis.c
+++ src/analysis.c
@@ -19,7 +19,40 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
-    abort();  // replace me with implementation.
+    //abort();  // replace me with implementation.
+
+    float Average_Waiting_Time
+    float Average_Turnaround_Time
+    unsigned long Total_Clock_Time
+    dyn_array_t * queue
+    ScheduleResult_t results
+
+    queue = load_process_control_blocks(argv[0]);
+
+    if(queue == NULL){
+	printf("Error Loading PCBS\n");
+	return EXIT_FAILURE;
+    switch(argv[1]){
+	case "FCFS":
+		first_come_first_serve(queue,results);
+		break;
+	case "SJF":
+		shortest_job_first(queue,results);
+		break;
+	case "RR":
+		round_robin(queue,results,argv[2]);
+		break;
+	case "SRTF":
+		shortest_remaining_time_first(queue,results);
+		break;
+	default:
+		return EXIT_FAILURE;
+		break;
+    }
+
+    printf("Average Waiting Time: %d \n", results.average_waiting_time);
+    printf("Average Turnaround Time: %d \n", results.average_turnaround_time);
+    printf("Total Clock Time: %d \n", results.total_run_time);
 
     return EXIT_SUCCESS;
 }
